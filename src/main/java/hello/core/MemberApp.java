b7c3ce48ff73de471c735f5366fff8084429c889
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2022-01-23 23:02:00 +0900

    스프링으로 전환하기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
@@ -7,17 +10,13 @@ import hello.core.member.MemberService;
 public class MemberApp {
 
 	public static void main(String[] args) {
-//		MemberService memberService = new MemberServiceImpl();
-//		Member member = new Member(1L, "memberA", Grade.VIP);
-//		memberService.join(member);
-//		
-//		Member findMember = memberService.findMember(1L);
-//		System.out.println("new member = " + member.getName());
-//		System.out.println("find Member = " + findMember.getName());
 		
-		AppConfig appConfig = new AppConfig();
+//		AppConfig appConfig = new AppConfig();
+//		MemberService memberService = appConfig.memberService();
 		
-		MemberService memberService = appConfig.memberService();
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+		
 		Member member = new Member(1L, "memberA", Grade.VIP);
 		memberService.join(member);
 		
