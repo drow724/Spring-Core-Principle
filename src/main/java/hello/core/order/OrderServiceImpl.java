b90c7dd68a7094a4a92c375af7e2bc86000742ae
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2022-02-01 01:02:22 +0900

    롬복과 최신 트렌드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,26 +1,27 @@
 package hello.core.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 
 @Component // ("service")
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
-	// private final MemberRepository memberRepository = new
-	// MemoryMemberRepository();
-	// private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-	// private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-	// private DiscountPolicy discountPolicy;
+//	private final MemberRepository memberRepository = new
+//	MemoryMemberRepository();
+//	private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//	private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+//	private DiscountPolicy discountPolicy;
 
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
 
-	// @Autowired private MemberRepository memberRepository;
-	// @Autowired private DiscountPolicy discountPolicy;
+//	@Autowired private MemberRepository memberRepository;
+//	@Autowired private DiscountPolicy discountPolicy;
 
 //	private MemberRepository memberRepository;
 //  private DiscountPolicy discountPolicy;
@@ -30,7 +31,7 @@ public class OrderServiceImpl implements OrderService {
 //		this.memberRepository = memberRepository;
 //		this.discountPolicy = discountPolicy;
 //	}
-
+//
 //	/**
 //	 * @param discountPolicy the discountPolicy to set
 //	 */
@@ -48,14 +49,14 @@ public class OrderServiceImpl implements OrderService {
 //		System.out.println("memberRepository = " + memberRepository);
 //		this.memberRepository = memberRepository;
 //	}
-
-	// 생성자가 1개면 의존성 자동 주입
-	@Autowired
-	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-		System.out.println("1. OrderServiceImpl.OrderServiceImpl");
-		this.memberRepository = memberRepository;
-		this.discountPolicy = discountPolicy;
-	}
+//
+// 	생성자가 1개면 의존성 자동 주입
+//	@Autowired
+//	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//		System.out.println("1. OrderServiceImpl.OrderServiceImpl");
+//		this.memberRepository = memberRepository;
+//		this.discountPolicy = discountPolicy;
+//	}
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
